77ab75af4b8565d10cef21933602a3f5f3bb7f2c
Klederson Bueno <klederson@klederson.com>
2013-11-18 16:24:23 -0200

    lint

--- src/angular-masonry-directive.js
+++ src/angular-masonry-directive.js
@@ -1,33 +1,32 @@
-(function(){
-  "use strict";
+(function() {
+    "use strict";
 
-  angular.module('masonry', ['ng']).directive('masonry', function ($parse) {
-    return {
-        restrict: 'AC',
-        link: function (scope, elem, attrs) {
-            scope.items = [];
-            var container = elem[0];
-            var options = angular.extend({
-                itemSelector : '.item'
-            }, JSON.parse(attrs.masonry));
+    angular.module('masonry', ['ng']).directive('masonry', function($parse) {
+        return {
+            restrict: 'AC',
+            link: function(scope, elem, attrs) {
+                scope.items = [];
+                var container = elem[0];
+                var options = angular.extend({
+                    itemSelector: '.item'
+                }, JSON.parse(attrs.masonry));
 
-            scope.obj = new Masonry( container, options);
-        }
-    };        
-  }).directive('masonryTile', function () {
-    return {
-        restrict: 'AC',
-        link: function (scope, elem, attrs) {
-            var master = elem.parent('*[mansory]:first').scope();
-            var mansory = master.obj;
+                scope.obj = new Masonry(container, options);
+            }
+        };
+    }).directive('masonryTile', function() {
+        return {
+            restrict: 'AC',
+            link: function(scope, elem) {
+                var master = elem.parent('*[mansory]:first').scope();
+                var mansory = master.obj;
 
-            elem.ready(function() {
-                mansory.addItems([elem])
-                mansory.reloadItems()
-                mansory.layout();
-            })
-            
-       }
-   };        
-  });
+                elem.ready(function() {
+                    mansory.addItems([elem]);
+                    mansory.reloadItems();
+                    mansory.layout();
+                });
+            }
+        };
+    });
 })();
