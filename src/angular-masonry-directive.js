1faa146184a8b408d124cddf4c3499e5578a9e1c
David Nunez <arizonatribe@users.noreply.github.com>
2015-01-11 10:45:58 -0700

    Added calls to reload items when brick is removed
    Calling reloadItems() and layout() after a brick is removed.
    Also added appendBricks method (I found it helps bricks load in better when combined with infinite scroll; a new batch comes in from the bottom rather than rushing in from the top).
    Also added an optional listener I find useful when I want to broadcast from outside of the directive a request to call layout() again.

--- src/angular-masonry-directive.js
+++ src/angular-masonry-directive.js
@@ -1,23 +1,23 @@
 (function() {
     "use strict";
 
-    angular.module('masonry', ['ng']).directive('masonry', function($parse) {
+    angular.module('masonry', ['ng']).directive('masonry', function($parse, $timeout) {
         return {
             restrict: 'AC',
             link: function(scope, elem, attrs) {
                 var container = elem[0];
                 var options = angular.extend({
                     itemSelector: '.item'
-                }, JSON.parse(attrs.masonry));
+                }, angular.fromJson(attrs.masonry));
 
                 var masonry = scope.masonry = new Masonry(container, options);
 
                 var debounceTimeout = 0;
                 scope.update = function() {
                     if (debounceTimeout) {
-                        window.clearTimeout(debounceTimeout);
+                        $timeout.cancel(debounceTimeout);
                     }
-                    debounceTimeout = window.setTimeout(function() {
+                    debounceTimeout = $timeout(function() {
                         debounceTimeout = 0;
 
                         masonry.reloadItems();
@@ -26,6 +26,22 @@
                         elem.children(options.itemSelector).css('visibility', 'visible');
                     }, 120);
                 };
+                
+                scope.removeBrick = function() {
+                    $timeout(function() {
+                        masonry.reloadItems();
+                        masonry.layout();
+                   }, 500);
+                };                
+                
+                scope.appendBricks = function(ele) {
+                    masonry.appended(ele);
+                };                
+                
+                scope.$on('masonry.layout', function() {
+                    masonry.layout();                 
+                });
+                
                 scope.update();
             }
         };
@@ -35,11 +51,21 @@
             link: function(scope, elem) {
                 elem.css('visibility', 'hidden');
                 var master = elem.parent('*[masonry]:first').scope(),
-                    update = master.update;
+                    update = master.update,
+                    removeBrick = master.removeBrick,
+                    appendBricks = master.appendBricks;                    
                 if (update) {
                     imagesLoaded( elem.get(0), update);
                     elem.ready(update);
                 }
+                if (appendBricks) {
+                    imagesLoaded( elem.get(0), appendBricks(elem));
+                }                
+                scope.$on('$destroy', function() {
+                    if (removeBrick) {
+                        removeBrick();
+                    }
+                });                
             }
         };
     });
